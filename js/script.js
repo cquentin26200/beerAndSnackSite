b9d746a09bb3938914115df450f3926045430c11
cquentin26200 <qcheronpedenschool.fr>
2022-05-30 11:46:34 +0200

    add files folders

---
+++ js/script.js
@@ -0,0 +1,51 @@
+/*async function getApi () {
+    try {
+        return {
+            beer: fetch('https://api.sampleapis.com/beers/ale')
+                .then(response => response.json())
+                .then(data => data),
+            food: fetch('https://themealdb.com/api/json/v1/1/filter.php?c=Seafood')
+                .then(response => response.json())
+                .then(data => data)
+        } 
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+async function useApi () {
+    const api = await getApi();
+    console.log(api)
+}
+
+useApi();*/
+
+const menuBurgerHeader = document.querySelector(".menuBurgerHeader");
+
+function showHideMenuBurgerHeader () {
+    if (menuBurgerHeader.classList.contains("hideMenuBurgerHeader")) {
+        menuBurgerHeader.classList.replace("hideMenuBurgerHeader", ["showMenuBurgerHeader"]);
+    } else {
+        menuBurgerHeader.classList.replace("showMenuBurgerHeader", ["hideMenuBurgerHeader"]);
+    }
+}
+
+const activeMenuBurgerHeader = document.querySelector(".activeMenuBurgerHeader");
+
+activeMenuBurgerHeader.addEventListener("click", showHideMenuBurgerHeader);
+
+//show all menu
+
+const activeMenu = document.querySelectorAll(".activeMenu");
+
+for (let i = 0; i < activeMenu.length; i++) {
+    activeMenu[i].addEventListener("click", () => {
+        if (activeMenu[i].parentElement.children[1].classList.contains("none")) {
+            activeMenu[i].parentElement.children[1].classList.replace("none", ["block"]);
+        } else {
+            activeMenu[i].parentElement.children[1].classList.replace("block", ["none"]);
+        }
+        console.log(activeMenu[i].parentElement.children[1].children)
+    })
+}
